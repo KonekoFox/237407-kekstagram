6366d928230cd787923656e509fedde4d9ea9a60
Юлия Исаева <isaeva-spb@inbox.ru>
2016-10-08 16:47:27 +0400

    Task1

--- src/js/resizer.js
+++ src/js/resizer.js
@@ -87,7 +87,7 @@
       // NB! Такие параметры сохраняются на время всего процесса отрисовки
       // canvas'a поэтому важно вовремя поменять их, если нужно начать отрисовку
       // чего-либо с другой обводкой.
-
+      
       // Толщина линии.
       this._ctx.lineWidth = 6;
       // Цвет обводки.
@@ -110,14 +110,46 @@
       // нужно отрисовать и координаты его верхнего левого угла.
       // Координаты задаются от центра холста.
       this._ctx.drawImage(this._image, displX, displY);
+      
+      // Оверлей
+      this._ctx.fillStyle = 'rgba(0, 0, 0, 0.8)';
+      this._ctx.beginPath();
+      this._ctx.moveTo(-this._container.width / 2, -this._container.height / 2);
+      this._ctx.lineTo(this._container.width / 2, -this._container.height / 2);
+      this._ctx.lineTo(this._container.width / 2, this._container.height / 2);
+      this._ctx.lineTo(-this._container.width / 2, this._container.height / 2);
+
+      this._ctx.moveTo(-this._resizeConstraint.side / 2 - this._ctx.lineWidth,
+                 -this._resizeConstraint.side / 2 - this._ctx.lineWidth);
+      this._ctx.lineTo(this._resizeConstraint.side / 2 - this._ctx.lineWidth / 2,
+                 -this._resizeConstraint.side / 2 - this._ctx.lineWidth);
+      this._ctx.lineTo(this._resizeConstraint.side / 2 - this._ctx.lineWidth / 2,
+                 this._resizeConstraint.side / 2 - this._ctx.lineWidth / 2);
+      this._ctx.lineTo(-this._resizeConstraint.side / 2 - this._ctx.lineWidth,
+                 this._resizeConstraint.side / 2 - this._ctx.lineWidth / 2);
+      this._ctx.closePath();
+      this._ctx.fill('evenodd');
+        
 
       // Отрисовка прямоугольника, обозначающего область изображения после
       // кадрирования. Координаты задаются от центра.
+        
       this._ctx.strokeRect(
           (-this._resizeConstraint.side / 2) - this._ctx.lineWidth / 2,
           (-this._resizeConstraint.side / 2) - this._ctx.lineWidth / 2,
           this._resizeConstraint.side - this._ctx.lineWidth / 2,
           this._resizeConstraint.side - this._ctx.lineWidth / 2);
+      
+      // Вывод размеров изображения
+      this._ctx.font = '14px Arial, sans-serif';
+      
+      var text = this._image.naturalWidth + ' x ' + this._image.naturalHeight;
+      var textWidth = this._ctx.measureText(text);
+      
+      this._ctx.fillStyle = "#ffffff";
+      this._ctx.fillText(text,
+                        -textWidth.width / 2,
+                        -this._resizeConstraint.side / 2 - this._ctx.lineWidth * 2.5);
 
       // Восстановление состояния канваса, которое было до вызова ctx.save
       // и последующего изменения системы координат. Нужно для того, чтобы
